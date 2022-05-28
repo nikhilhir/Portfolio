8e40ec690582984652645621bd37179429a65b0d
Nikhil Hirulkar <97525857+nikhilhir@users.noreply.github.com>
2022-05-28 18:01:24 +0530

    update

--- myapp/src/components/Project.js
+++ myapp/src/components/Project.js
@@ -9,27 +9,35 @@ const Project = () => {
   const [state] = React.useState([
     {
       id: 1,
-      heading: "American Eagle Clone",
-      img: "./images/american.png",
-      link: "https://rubyroy21.github.io/American-Eagle/",
-      repolink: "https://github.com/Rohit8483/American-Eagle",
-      p:"American Eagle is a leading global specialty retailer offering high-quality, on-trend clothing and accessories at affordable prices. We try to build a American Eagle clone using Html, CSS, and JavaScript"
+      heading: "Bewakoof.com Clone",
+      img: "./images/Bewakoof.png",
+      link: "https://cloneofbewakoof.netlify.app/",
+      repolink: "https://github.com/nikhilhir/Bewakoof.com--Project-?organization=nikhilhir&organization=nikhilhir",
+      p:"This is a clone of Bewakoof.com.is a E-Commerce website for Clothes and Accessories, on-trend clothing at affordable prices. We try to build a Bewakoof.com clone using Html, CSS, and JavaScript"
     },
     {
       id: 2,
-      heading: "Smallcase Clone",
-      img: "./images/smallcase.png",
-      link: "https://smallcase-clone.netlify.app/",
-      repolink: "https://github.com/Rohit8483/smallcase",
-      p:"smallcase is modern investment portfolio that help users build a low cost, secure, long term & diversified portfolio, we have cloned using Html, CSS, and JavaScript and json-server"
+      heading: "Nordstromrack Clone",
+      img: "./images/NordstromRack.png",
+      link: "https://lustrous-eclair-2f1c6d.netlify.app/index.html",
+      repolink: "https://github.com/nikhilhir/Nordstromrack_clone",
+      p:"A US-based e-commerce website.where users can purchase clothes, accessories, household, and lifestyle products. we have cloned using Html, CSS, and JavaScript and json-server"
     },
     {
       id: 3,
-      heading: "Airbnb Clone",
-      img: "./images/front.png",
-      link: "https://airbnb-clone-snowy-three.vercel.app/",
-      repolink: "https://github.com/Rohit8483/Airbnb-Clone",
-      p:"Airbnb is an American company that operates an online marketplace for lodging, primarily homestays for vacation rentals, and tourism activities. We build this website using ReactJS. "
+      heading: "Be Bodywise Clone",
+      img: "./images/BeBodywise.png",
+      link: "#",
+      repolink: "https://github.com/nikhilhir/TeamFriends",
+      p:"Be Bodywise is a website provides diagnostics and e-pharmacy.It is e-commerce where users purchase health care products. plus free online consultation from the team of doctors for women’s health &wellness. "
+    },
+    {
+      id: 4,
+      heading: "TATA 1mg Clone",
+      img: "./images/TATA_1mg.png",
+      link: "https://relaxed-mousse-6ea758.netlify.app/",
+      repolink: "https://github.com/nikhilhir/clone1mg",
+      p:"Tata 1mg, is a healthcare platform based in Gurgaon, India. It provides services, including e-pharmacy, diagnostics, e-consultation and health content.we have cloned using Html, CSS, and JavaScript,React,Redux and json-server "
     },
   ]);
   return (
